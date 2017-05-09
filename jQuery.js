9f8fb33f9d0dd31804a5683bd4a85cea626a51cf
Natsu <natsukichan13@hotmail.fr>
2017-05-09 13:41:31 -0400

    clear button

---
+++ jQuery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  for(var i = 0; i < 16; i++) {
+    body.append($('div'));
+  }
+  });
+
+  $(document).ready(function() {
+    $(document).on('click', function() {
+        alert('est-ce que ça marche ?');
+    });
+});
