4149e806b4bc8300655185a8122b74bf63593cc7
Natsu <natsukichan13@hotmail.fr>
2017-05-10 11:50:20 -0400

    making it faster

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
 
-  var makeGrill = function() {
-    $('#square').append("<div></div>");
-    $('div').hover(function() {
-        $(this).addClass('highlight');
+    var bigGrill = $('#square');
+
+    var makeGrill = function(size) {
+      var cellSize = 480 / size;
+      var cell = $('<div style="height: '+ cellSize +'px; width: '+ cellSize +'px"></div>');
+      bigGrill.append(cell);
+      cell.hover(function() {
+          $(this).addClass('highlight');
+      });
+    };
+
+    for(var i = 0; i < 16; i++) {
+        for(var y = 0; y < 16; y++) {
+            makeGrill(16);
+        };
+    };
+
+    $('.clear').on('click', function() {
+        $('div').removeClass('highlight');
+        bigGrill.children($('div')).detach();
+        var grillSize = prompt("Quelle taille de grille souhaitez-vous (entre 1 et 200 cases) ?");
+        for(var i = 0; i < grillSize; i++) {
+            for(var y = 0; y < grillSize; y++) {
+                makeGrill(grillSize);
+            };
+        };
     });
-  };
-
-  for(var i = 1; i < 17; i++) {
-      for(var y = 1; y < 17; y++) {
-          makeGrill();
-      };
-  };
-
-  $('.clear').on('click', function() {
-      $('div').removeClass('highlight');
-      $('#square').children($('div')).detach();
-      var grillSize = prompt("Quelle taille de grille souhaitez-vous (nombre de cases) ?");
-      var resize = function () {
-          $('div').width(480 / grillSize).height(480 / grillSize);
-      };
-      for(var i = 0; i < grillSize; i++) {
-          for(var y = 0; y < grillSize; y++) {
-                makeGrill();
-                resize();
-          };
-      };
-  });
 });
