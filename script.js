3fa69425d5198e32f28dd2b9dd0544a67a26a606
Natsu <natsukichan13@hotmail.fr>
2017-05-10 10:47:31 -0400

    javascript over

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-
-
-
 $(document).ready(function() {
-for(var i = 1; i < 17; i++) {
-      for(var y = 1; y < 17; y++) {
-          $('#square').append("<div><p>" + y + "</p></div>");
-      };
-    };
 
+  var makeGrill = function() {
+    $('#square').append("<div></div>");
     $('div').hover(function() {
         $(this).addClass('highlight');
     });
+  };
 
-    $('.clear').on('click', function() {
+  for(var i = 1; i < 17; i++) {
+      for(var y = 1; y < 17; y++) {
+          makeGrill();
+      };
+  };
+
+  $('.clear').on('click', function() {
       $('div').removeClass('highlight');
-    });
-
-
-
-
-
-
-
-
+      $('#square').children($('div')).detach();
+      var grillSize = prompt("Quelle taille de grille souhaitez-vous (nombre de cases) ?");
+      var resize = function () {
+          $('div').width(480 / grillSize).height(480 / grillSize);
+      };
+      for(var i = 0; i < grillSize; i++) {
+          for(var y = 0; y < grillSize; y++) {
+                makeGrill();
+                resize();
+          };
+      };
+  });
 });
